a1940d55351b062211f0e1840f7ab23ec969f1e2
ThomasMousseau <thom.mousseau@gmail.com>
2022-02-11 22:10:18 -0500

    creation services and classes

---
+++ Models/Topic.cs
@@ -0,0 +1,16 @@
+using Article;
+
+public class Topic
+{
+    public int importance;
+    public string title;
+    public Articles[] articles;
+}
+
+
+/*import { Article } from "./article.model";
+export interface Topic {
+    title: string;
+    importance: number;
+    artciles: Article[];
+} */
